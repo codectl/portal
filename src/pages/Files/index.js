a9c64dd0777f113c02e064ea836d634ab2e576cc
Renato Teixeira <rena2damas@gmail.com>
2022-04-28 14:32:24 +0100

    Add files page

---
+++ src/pages/Files/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "layouts/base"
+
+
+const Files = () =>
+    <Layout title="Files" description="Files page">
+        <div>Files</div>
+    </Layout>
+
+export default Files
